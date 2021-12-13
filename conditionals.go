505544a81a17637ddf07cf81824330cd6e786347
Jaminur-Rashid <jaminurrashid21@gmail.com>
2021-12-13 13:13:04 +0600

    add variables.go and condionals.go

---
+++ conditionals.go
@@ -0,0 +1,17 @@
+/*
+    Author : Jaminur Rasid 
+	Date   : 13-12-2021
+*/
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Variables decleration
+	*/
+	my
+	if myAge%2 == 0{
+		fmt.Println("")
+	}
+}
---
+++ variables.go
@@ -0,0 +1,28 @@
+/*
+    Author : Jaminur Rasid 
+	Date   : 13-12-2021
+*/
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Variables decleration
+	*/
+	var name = "jaminur"
+	var id = "Ce-16034"
+	myUniversity := "MBSTU"
+	var deptName string = "CSE"
+	var age int = 25
+	floatNumber := 233.44
+	var numTwo int = 9
+	fmt.Println("name is : ", name, "id is : ", id)
+	fmt.Println("My Department is : ", deptName)
+	fmt.Println("My age is : ", age)
+	fmt.Printf("Type of floatNumber is %T", floatNumber)
+	fmt.Println(floatNumber)
+	fmt.Println(numTwo)
+	fmt.Println("Is it Ok..?")
+	fmt.Println("My University is ", myUniversity)
+}
